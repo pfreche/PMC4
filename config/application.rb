d6b62ce0aa4309b471e296548e84db54117821d1
Peter <pfreche@gmail.com>
2017-09-17 10:58:43 -0400

    merge mdl to master

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module PMC4
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-   config.web_console.whitelisted_ips = '192.168.99.1'
+   config.web_console.whitelisted_ips = '192.168.0.0/16'
   end
 end
